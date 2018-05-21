549ee55d455f0e0d1ccc81a21cc21b3302295aa5
fullcorder <fullcorder@gmail.com>
2018-05-21 21:54:46 +0900

    Add IsSupported function

--- Assets/HapticPlugin/Scripts/iOS/HapticPlugin.iOS.cs
+++ Assets/HapticPlugin/Scripts/iOS/HapticPlugin.iOS.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 namespace Haptic
 {
-    public static partial class HapticPlugin
+    public static class HapticPlugin
     {
         private static bool? _isSupported;
 
@@ -15,11 +15,16 @@ namespace Haptic
 
         public static void Generate(FeedBackType type)
         {
-            if(!_isSupported.HasValue) _isSupported = _Haptic_supported();
+            if(!_isSupported.HasValue) _isSupported = IsSupported();
 
             if(!_isSupported.Value) return;
 
             _Haptic_generate((int) type);
+        }
+
+        public static bool IsSupported()
+        {
+            return _Haptic_supported();
         }
     }
 }
